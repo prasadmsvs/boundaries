91b3cefcdcc6b1a81d8d8af82d2baf0ed6168b9a
Prasad S Manthni <pmanthni@in.ibm.com>
2016-06-27 18:01:04 +0530

    added mysql file

---
+++ boundaries_db_sql.sql
@@ -0,0 +1,180 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.17 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for boundaries
+CREATE DATABASE IF NOT EXISTS `boundaries` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `boundaries`;
+
+
+-- Dumping structure for table boundaries.features
+CREATE TABLE IF NOT EXISTS `features` (
+  `id` int(11) NOT NULL,
+  `feature` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `feature_UNIQUE` (`feature`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.features: ~0 rows (approximately)
+/*!40000 ALTER TABLE `features` DISABLE KEYS */;
+/*!40000 ALTER TABLE `features` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.features_has_property_types
+CREATE TABLE IF NOT EXISTS `features_has_property_types` (
+  `features_id` int(11) NOT NULL,
+  `property_types_id` int(11) NOT NULL,
+  PRIMARY KEY (`features_id`,`property_types_id`),
+  KEY `fk_features_has_property_types_property_types1_idx` (`property_types_id`),
+  KEY `fk_features_has_property_types_features1_idx` (`features_id`),
+  CONSTRAINT `fk_features_has_property_types_features1` FOREIGN KEY (`features_id`) REFERENCES `features` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_features_has_property_types_property_types1` FOREIGN KEY (`property_types_id`) REFERENCES `property_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.features_has_property_types: ~0 rows (approximately)
+/*!40000 ALTER TABLE `features_has_property_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `features_has_property_types` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.locations
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.locations: ~0 rows (approximately)
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+REPLACE INTO `locations` (`id`, `name`) VALUES
+	(1, 'India');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.permissions
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL,
+  `permission` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.property
+CREATE TABLE IF NOT EXISTS `property` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(225) NOT NULL,
+  `location` int(11) DEFAULT NULL,
+  `user` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `location_id_idx` (`location`),
+  KEY `user_id_idx` (`user`),
+  CONSTRAINT `location_id` FOREIGN KEY (`location`) REFERENCES `locations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.property: ~3 rows (approximately)
+/*!40000 ALTER TABLE `property` DISABLE KEYS */;
+REPLACE INTO `property` (`id`, `name`, `location`, `user`, `description`) VALUES
+	(1, 'my home', 1, 1, 'my description'),
+	(2, 'property 2', 1, 1, 'pro'),
+	(3, 'property 3', 1, 1, 'prolsjslfj');
+/*!40000 ALTER TABLE `property` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.property_types
+CREATE TABLE IF NOT EXISTS `property_types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `description_UNIQUE` (`description`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.property_types: ~0 rows (approximately)
+/*!40000 ALTER TABLE `property_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `property_types` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.property_types_has_property
+CREATE TABLE IF NOT EXISTS `property_types_has_property` (
+  `property_types_id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL,
+  PRIMARY KEY (`property_types_id`,`property_id`),
+  KEY `fk_property_types_has_property_property1_idx` (`property_id`),
+  CONSTRAINT `fk_property_types_has_property_property1` FOREIGN KEY (`property_id`) REFERENCES `property` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_property_types_has_property_property_types1` FOREIGN KEY (`property_types_id`) REFERENCES `property_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.property_types_has_property: ~0 rows (approximately)
+/*!40000 ALTER TABLE `property_types_has_property` DISABLE KEYS */;
+/*!40000 ALTER TABLE `property_types_has_property` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `password` varchar(225) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `address` varchar(225) DEFAULT NULL,
+  `user_types_id` int(11) NOT NULL,
+  `authKey` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_user_user_types1_idx` (`user_types_id`),
+  CONSTRAINT `fk_user_user_types1` FOREIGN KEY (`user_types_id`) REFERENCES `user_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.user: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+REPLACE INTO `user` (`id`, `first_name`, `last_name`, `password`, `email`, `address`, `user_types_id`, `authKey`) VALUES
+	(1, 'admin', 'admin', 'admin', 'admin@boundaries.in', NULL, 1, '123456');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.user_types
+CREATE TABLE IF NOT EXISTS `user_types` (
+  `id` int(11) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `type_UNIQUE` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.user_types: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user_types` DISABLE KEYS */;
+REPLACE INTO `user_types` (`id`, `type`) VALUES
+	(1, 'Admin');
+/*!40000 ALTER TABLE `user_types` ENABLE KEYS */;
+
+
+-- Dumping structure for table boundaries.user_types_has_permissions
+CREATE TABLE IF NOT EXISTS `user_types_has_permissions` (
+  `user_types_id` int(11) NOT NULL,
+  `permissions_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_types_id`,`permissions_id`),
+  KEY `fk_user_types_has_permissions_permissions1_idx` (`permissions_id`),
+  KEY `fk_user_types_has_permissions_user_types1_idx` (`user_types_id`),
+  CONSTRAINT `fk_user_types_has_permissions_permissions1` FOREIGN KEY (`permissions_id`) REFERENCES `permissions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_types_has_permissions_user_types1` FOREIGN KEY (`user_types_id`) REFERENCES `user_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table boundaries.user_types_has_permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user_types_has_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_types_has_permissions` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
